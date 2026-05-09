58ac960cc1be502f5c7c6b1b6f0865eb41130f5e
miasmicvoid <miasmicvoid@protonmail.com>
2026-05-10 01:32:40 +0200

    Added prerequisities install and package list

---
+++ installPrerequisities.sh
@@ -0,0 +1,6 @@
+sudo pacman -S --needed git base-devel
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+
+yay -S --needed sway swaybg swaylock swayidle swaync swayosd eww wofi waybar nautilus kitty spotify_player grim swappy 
