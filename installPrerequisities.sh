e870d4cbf015ac912d1c142961455fa9df275327
miasmicvoid <miasmicvoid@protonmail.com>
2026-05-12 21:06:05 +0200

    FIX: installPrerequisities now installs additional software

--- installPrerequisities.sh
+++ installPrerequisities.sh
@@ -4,3 +4,4 @@ cd yay
 makepkg -si
 
 yay -S --needed sway swaybg swaylock swayidle swaync swayosd eww wofi waybar nautilus kitty spotify-player grim swappy ly
+yay -S --needed pavucontrol nm-applet
