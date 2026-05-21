ed6e4fde30325a644c1d7fa25fb89c7b968bf504
miasmicvoid <miasmicvoid@protonmail.com>
2026-05-22 00:27:20 +0200

    [FEATURE]: Atuin added as prerequisity

--- installPrerequisities.sh
+++ installPrerequisities.sh
@@ -3,4 +3,4 @@ git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si
 
-yay -S --needed sway swaybg swaylock swayidle swaync swayosd eww wofi waybar nautilus kitty spotify-player grim swappy ly
+yay -S --needed sway swaybg swaylock swayidle swaync swayosd eww wofi waybar nautilus kitty spotify-player grim swappy ly atuin
